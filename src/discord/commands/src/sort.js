28f5e08a44f6b1a1f9e06196683bb8c2b9b9d396
Paul Wayper <paulway@mabula.net>
2022-02-20 15:46:09 +1100

    Updating sort command to Discord 13 and other improvements
    * A bit more logging for the moment while we get used to how things work.
    * `parentID` field renamed to `parentId`.
    * Use `GUILD_VOICE` and `GUILD_TEXT` channel types.
    * Use the common `setFafId` helper function when finding people in the game
      (both from the initial f/sort message and when we find matches from player
      names to discord names).
    * Exit early (for less indenting) if we can't find the requester's FAF ID.
    * Don't attempt to sort the player if their game has ended.
    * It's possible for a player to create a new game with the same name as the
      previous game they created, while some people are still in that voice
      channel (e.g. they haven't left yet).  So we try to find a channel with that
      name first, and if it doesn't exist we create it.  That may mean that some
      people get moved into the channel occupied by another game's players (if
      game A hasn't finished, the voice and game A' starts), but it's better than
      just dying.
    * Try to gather up all the players we don't know about for a message after
      we've sorted everyone.  Not working yet, not sure why.
    
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- src/discord/commands/src/sort.js
+++ src/discord/commands/src/sort.js
@@ -10,125 +10,170 @@ module.exports = {
     help: 'This will get everyone in your current match (which must have started) into separate voice channels based on team. \nUsage: `f/sort`',
     check: content => content.match(/^sort(.+)?/),
     run: async (msg, client) => {
+        console.log("Got to start of sort routine");
+
         try{
 
             let active_channel = await helper.getUserActiveVoiceChannel(msg);
             if (!active_channel) {
-                msg.channel.send(`You must be in a voice channel to run that command.`);
+                console.log(`${msg.author.username} not in voice channel`);
+                msg.channel.send("You must be in a voice channel to run that command.");
                 return;
             }
-            // console.log(active_channel.parentID)
-            console.log('active_channel', active_channel.id, active_channel.parentID);
-            if (process.env.CATEGORY_RESTRICTION_ID && active_channel.parentID !== process.env.CATEGORY_RESTRICTION_ID + '') {
+            // console.log(active_channel.parentId)
+            console.log('active_channel', active_channel.id, 'parent', active_channel.parentId);
+            // console.log('full channel details:', active_channel);
+            if (process.env.CATEGORY_RESTRICTION_ID && active_channel.parentId !== process.env.CATEGORY_RESTRICTION_ID + '') {
                 msg.channel.send(`You must be in the "Battle Prep" channel to run this`);
                 return;
             }
 
             let faf_id = await helper.getFafId(msg.author);
             if (!faf_id) {
+                console.log("FAF ID of", msg.author.username, "returned", faf_id, ", searching");
                 faf_id = await faf.searchUser(msg.author.username);
                 if (faf_id) {
-                    let user = FafUser.create({
-                        discord_id: msg.author.id,
-                        guild_id: msg.guild.id,
-                        faf_id: faf_id
-                    });
+                    await helper.setFafId(msg.author.id, faf_id, msg.guild.id, msg.author.username);
                 } else {
-                    msg.reply("I couldn't find your FAF username. Please set it, eg `f\/set antz` ")
+                    console.log(`Couldn't find FAF username for ${msg.author.username}`);
+                    msg.reply("I couldn't find your FAF username. Please set it, eg `f\/set " + msg.author.username + '`')
+                    return;
                 }
             }
-            console.log('searching for match', faf_id);
-            if (faf_id) {
-                // let bot_promise = msg.channel.send('Tracking your match')
-                let player_match = await faf.getPlayerCurrentMatch(faf_id);
-                if (player_match) {
-                    let match = await faf.getMatch(player_match.id);
-                    console.log('match', match.id);
-                    if (match && match.teams && match.teams.length) {
-                        let size = false
-                        if (match.map) {
-                            size = (match.map.width / 51.2) + 'x' + (match.map.height / 51.2);
-                        }
-                        let team_fields = match.teams.map(team => {
-                            return {
-                                name: 'Team ' + team.team,
-                                value: helper.getObjectValues(team.players, 'name').join('\n'),
-                                inline: true
-                            }
-                        });
-                        let main_fields = [{
-                            name: 'Victory Condition',
-                            value: match.victoryCondition,
-                            inline: true
-                        }];
-                        if (size) {
-                            main_fields.push({
-                                name: 'Size',
-                                value: size,
-                                inline: true
-                            })
-                        }
-                        main_fields.push({ name: '\u200B', value: '\u200B' })
-                        let report_fields = [...main_fields,...team_fields];
-                        await helper.processArray(match.teams, async function(team){
-                            let channel_name = `Team ${team.team} - ${match.name} (temp)`
-                            let parent_id = active_channel.parentID
-
-                            if (parseInt(msg.guild.id) === 657376549108187163 && process.env.CHANNEL_PARENT_ID) {
-                                parent_id = process.env.CHANNEL_PARENT_ID
-                            }
-                            let channel = await msg.channel.guild.channels.create(channel_name, {
-                                type: 'voice',
-                                reason: 'temp channel for a FAF game',
-                                parent: parent_id
-                            });
-                            let player_ids = helper.getObjectValues(team.players, 'id')
-                            let faf_users = FafUser.findAll({ where: {
-                                faf_id: player_ids,
-                                guild_id: msg.guild.id
-                            }});
-                            team.players.forEach(player => {
-                                let found = false;
-                                if (faf_users && faf_users.length) {
-                                    faf_users.forEach(faf_user => {
-                                        if (faf_user.faf_id === player.id) {
-                                            found = true;
-                                            helper.moveUser(client, msg.channel.guild.id, msg.author.id, channel.id);
-                                        }
-                                    })
-                                }
-                                if (!found) {
-                                    active_channel.members.array().forEach(member => {
-                                        if (member.displayName.toLowerCase() === player.name.toLowerCase()) {
-                                            helper.moveUser(client, msg.channel.guild.id, member.id, channel.id);
-                                        }
-                                    })
-                                }
-                            })
-                        });
-                        let description = 'Map generator match'
-                        if (match.map && match.map.description) { //map generator doesn't include the map object
-                            description = match.map.description
-                        }
-
-                        let embed = {
-                            description: description,
-                            fields: report_fields,
-                        };
-                        if (match.map && match.map.thumbnailUrlLarge) {
-                            embed.thumbnail = {url: encodeURI(match.map.thumbnailUrlLarge)};
-                            embed.image = {
-                                url: encodeURI(match.map.thumbnailUrlLarge)
-                            }
-                        }
-                        await helper.sendLog(msg.guild.id, {embed}, msg);
-                    } else {
-                        msg.channel.send(`I couldn't find the players in the match.`);
-                    }
-
-                } else {
-                    msg.channel.send("I couldn't find your match");
+            if (! faf_id) {
+                console.log(`Couldn't find FAF ID for ${$msg.author_username}`);
+                msg.channel.send("I couldn't find your match");
+                return;
+            };
+            console.log('searching for last match for ', faf_id);
+            // let bot_promise = msg.channel.send('Tracking your match')
+            let player_match = await faf.getPlayerCurrentMatch(faf_id);
+            if (player_match) {
+                if (player_match.attributes.endTime !== null) {
+                    msg.channel.send("You're not in a current match (last match ended " + player_match.attributes.endTime + ")");
+                    return;
                 }
+            } else {
+                msg.channel.send("I couldn't find the players in the match.");
+                return;
+            };
+            msg.channel.send("Hi " + msg.author.username + ", you're in match " + player_match.id);
+            console.log('player is in match', player_match.id);
+            let match = await faf.getMatch(player_match.id);
+            console.log('match', match.id);
+            if (match && match.teams && match.teams.length) {
+                let size = false
+                if (match.map) {
+                    size = (match.map.width / 51.2) + 'x' + (match.map.height / 51.2);
+                }
+                let team_fields = match.teams.map(team => {
+                    return {
+                        name: 'Team ' + team.team,
+                        value: helper.getObjectValues(team.players, 'name').join('\n'),
+                        inline: true
+                    }
+                });
+                let main_fields = [{
+                    name: 'Victory Condition',
+                    value: match.victoryCondition,
+                    inline: true
+                }];
+                if (size) {
+                    main_fields.push({
+                        name: 'Size',
+                        value: size,
+                        inline: true
+                    })
+                }
+                main_fields.push({ name: '\u200B', value: '\u200B' })
+                let report_fields = [...main_fields,...team_fields];
+                let unknown_players = [];
+                await helper.processArray(match.teams, async function(team){
+                    console.log("Team data:", team);
+                    let channel_name = `Team ${team.team} - ${match.name} (temp)`
+                    let parent_id = active_channel.parentId
+
+                    if (parseInt(msg.guild.id) === 657376549108187163 && process.env.CHANNEL_PARENT_ID) {
+                        parent_id = process.env.CHANNEL_PARENT_ID
+                    }
+                    let channel_exists = await msg.channel.guild.channels.cache.find(
+                        channel => channel.name === channel_name
+                    );
+                    let channel;
+                    if (channel_exists) {
+                        channel = channel_exists;
+                    } else {
+                        channel = await msg.channel.guild.channels.create(channel_name, {
+                            type: 'GUILD_VOICE',
+                            reason: 'temp channel for a FAF game',
+                            parent: parent_id
+                        });
+                        console.log("Created new channel", channel.id, "named", channel_name);
+                    // } else {
+                    //     msg.reply(`I've found channel ${channel_name} for you, moving you there`);
+                    //     channel = channel_exists;
+                    //     helper.moveUser(client, channel.guild.id, msg.author.id, channel.id);
+                    //     console.log(`Channel ${channel_name} existed, moved ${msg.author.username} straight there.`);
+                    }
+                    let player_ids = helper.getObjectValues(team.players, 'id')
+                    console.log("players to sort into this channel:", player_ids);
+                    let faf_users = await FafUser.findAll({ where: {
+                        faf_id: player_ids,
+                        guild_id: msg.guild.id
+                    }});
+                    console.log("faf users for those players:", faf_users);
+                    team.players.forEach(player => {
+                        console.log("player", player);
+                        let found = false;
+                        // if (faf_users && faf_users.length) {
+                        faf_users.forEach(faf_user => {
+                            console.log("... faf_user", faf_user.faf_id, "==", player.id, "?");
+                            if (faf_user.faf_id == player.id) {
+                                found = true;
+                                helper.moveUser(client, msg.channel.guild.id, msg.author.id, channel.id);
+                            }
+                        });
+                        // }
+                        if (!found) {
+                            console.log("No database match for player, trying to find name in active channel") 
+                            active_channel.members.forEach(member => {
+                                if (member.displayName.toLowerCase() === player.name.toLowerCase()) {
+                                    user = helper.setFafId(member.id, player.id, msg.channel.guild.id, member.displayName);
+                                    found = true;
+                                    helper.moveUser(client, msg.channel.guild.id, member.id, channel.id);
+                                }
+                            });
+                            if (!found) {
+                                console.log("... couldn't find player", player.name, "in channel, sending f/set message");
+                                // msg.channel.send(`I couldn't find a discord ID for FAF player '${player.name}' - if that's you, issue \`f\/set ${player.name}\` to fix this`);
+                                unknown_players.push(player.name);
+                            }
+                        }
+                    })
+                });
+                if (unknown_players.length) {
+                    await msg.channel.send(
+                        "I couldn't find Discord usernames for the following FAF players: " +
+                        unknown_players.join(', ') +
+                        " - if you're one of those people, issue `f/set` with your FAF username."
+                    );
+                }
+                let description = 'Map generator match'
+                if (match.map && match.map.description) { //map generator doesn't include the map object
+                    description = match.map.description
+                }
+
+                let embed = {
+                    description: description,
+                    fields: report_fields,
+                };
+                if (match.map && match.map.thumbnailUrlLarge) {
+                    embed.thumbnail = {url: encodeURI(match.map.thumbnailUrlLarge)};
+                    embed.image = {
+                        url: encodeURI(match.map.thumbnailUrlLarge)
+                    }
+                }
+                await helper.sendLog(msg.guild.id, {embed}, msg);
             }
         } catch (e) {
             console.log('caught exception', e);
