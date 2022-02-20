c2a1045c30f2f28e24e37d7762e89f0147ab45db
Paul Wayper <paulway@mabula.net>
2022-02-20 15:47:05 +1100

    Updating for Discord.js v13
    * Need to supply bot intents when we start the client.
    * Don't do things with the Guild just yet - just handle messageCreate and
      VoiceStateUpdate.
    
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- src/discord/discord.js
+++ src/discord/discord.js
@@ -1,20 +1,25 @@
 
 let helper = require('../common/helper');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({intents: [
+    Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES,
+]});
 const eventHandlers = require('./event-handlers')
 
 function start(){
 
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
+        // const channel = client.channels.cache.get("720187277355122769");
     });
-    client.on('message', eventHandlers.onMessage);
+    client.on('messageCreate', eventHandlers.onMessage);
     client.on('voiceStateUpdate', eventHandlers.onVoiceStateUpdate);
-    client.on("guildCreate", eventHandlers.onGuildCreate)
-    client.on("guildDelete", eventHandlers.onGuildDelete)
-    client.on("guildMemberAdd", eventHandlers.onGuildMemberAdd)
-    client.on("guildMemberRemove", eventHandlers.onGuildMemberRemove)
+    // client.on("guildCreate", eventHandlers.onGuildCreate);
+    // client.on("guildDelete", eventHandlers.onGuildDelete);
+    // client.on("guildMemberAdd", eventHandlers.onGuildMemberAdd);
+    // client.on("guildMemberRemove", eventHandlers.onGuildMemberRemove);
+    // console.log("Token:", process.env.DISCORD_TOKEN);
     client.login(process.env.DISCORD_TOKEN);
 
 }
