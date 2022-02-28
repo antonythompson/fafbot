c5a84cac5c26e097925f343085b2e23858f7edc1
Paul Wayper <paulway@mabula.net>
2022-02-28 23:05:12 +1100

    Don't need to debug getFafId and processArray now we've fixed those problems
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -8,13 +8,10 @@ const GuildJoin = models.GuildJoin;
 let getFafId = async (discord_author) => {
     console.log("getFafId(", discord_author.username, ") user id ", discord_author.id);
     if (discord_author.id == null) {
-        console.log("... bad id?");
         return null;
     }
     try{
-        console.log("foo!");
         let user = await fafUserModel.findOne({where: {discord_id: discord_author.id}});
-        console.log("... user:", user);
         if (user) {
             return user.faf_id;
         }
@@ -110,10 +107,6 @@ const findOrCreateGuild = async guild => {
 }
 
 const processArray = <T>(items: T[], process: (value: T, index?: string) => void): Promise<void> => {
-    console.log("processArray got items:", items);
-    if (items == null) {
-        items = Array();
-    }
     return new Promise((resolve, reject) => {
         let todo = items.concat();
         const fn = () => {
